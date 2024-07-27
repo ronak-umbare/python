cd11d7dad2891ff10e6ce7d68d84e70c2a7ed1e1
ronak-umbare <ronak21105@gmail.com>
2024-07-27 22:26:32 +0530

    An UI-less file manager is here

--- os.py
+++ os.py
@@ -1,12 +1,5 @@
 import os
 
-# # if(not os.path.exists("Data")):
-# #     os.mkdir("Data")
-
-# for i in range(0,100):
-#     os.rename(f"Data/SampleFile_{i+1}",f"Data/SampleFolder_{i+1}")
-    
-    
 def create_file(filename):
     try:
         with open(filename, 'x') as f:
@@ -43,7 +36,58 @@ def read(filename):
     except Exception as E:
         print("An error occured")
         
-
-    
+def edit(filename):
+    try:
+        with open(filename, 'a') as f:
+            content = input("Enter what do u wanna add : ")
+            f.write(content + "\n") 
+            print('Content added to {filename}')
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as E:
+        print("An error occured")
+        
+def main():
+    while True:
+        print("FILE MANAGER \n")
+        print("Press 1: to create \n")
+        print("Press 2: to view \n")
+        print("Press 3: to Delete \n")
+        print("Press 4: to Read \n")
+        print("Press 5: to Edit \n")
+        print("Press 6: to Exit \n")
+        
+        choice = input("Enter UR choice : ")
+         
+        if choice == "1":
+            filename = input("Enter filename to create: ")
+            create_file(filename)
             
+        elif choice == "2":
+            view_files()
+        
+        elif choice == "3":
+            filename = input("Enter filename u wanna delete: ")
+            delete()
             
+        elif choice == "4":
+            filename = input("Enter the file name u wanna read: ")
+            read(filename)
+        
+        elif choice == "5":
+            filename = ("Enter the file name which you wanna edit: ")
+            edit(filename)
+            
+        elif choice == "6":
+            print("Closing...")
+            break
+            
+        else:
+            print("Invalid choice")
+            
+if __name__ == "__main__":
+    main()
+        
+        
+        
+        
