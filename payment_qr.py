962bc65b14f8f4fa4df6ec9422246632b4740c24
ronak-umbare <ronak21105@gmail.com>
2024-07-29 16:24:20 +0530

    Created an qr maker while doing qrcode

---
+++ payment_qr.py
@@ -0,0 +1,20 @@
+import qrcode
+
+upi_id = input("Enter ur UPI ID = ")
+
+# upi://pay?pa=UPI_ID&pn=NAME&am=Amount&cu=CURRENCY&tn=MESSAGE'
+#Defining the payment URLs based on payment method
+
+phonepay = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+gpay = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+#creating qr's
+phonepayQr = qrcode.make(phonepay)
+paytmQR = qrcode.make(paytm)
+gpayQR = qrcode.make(gpay)
+
+#saving
+phonepayQr.show("phpe.png")
+paytmQR.show("payt.png")
+gpay.show("gpay.png")
